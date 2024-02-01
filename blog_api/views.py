2bad2f6467f882fcb085a7889b0a002d0f6e9d43
urbain32 <urbainq32@gmail.com>
2024-02-01 08:53:50 +0200

    generics to retrieve and delete api view

--- blog_api/views.py
+++ blog_api/views.py
@@ -6,7 +6,7 @@ class PostList(generics.ListCreateAPIView):
     queryset = Post.postobjects.all() # e return all post data that are published only.
     serializer_class = PostSerializer
 
-
+# generics to retrieve and delete api view
 class PostDetail(generics.RetrieveDestroyAPIView):
     queryset = Post.objects.all() # e return all post data.
     serializer_class = PostSerializer
