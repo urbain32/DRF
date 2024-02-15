e61eda93f8ea00b25d2f7f34e02815153c2e16ac
urbain32 <urbainq32@gmail.com>
2024-02-15 11:37:59 +0200

    # function to create our test and run them

--- blog/tests.py
+++ blog/tests.py
@@ -10,4 +10,13 @@ class Test_Create_Post(TestCase):
         test_category= Category.objects.create(name="django")
         tetsuser1 = User.objects.create_user(username="test_user1",password="123456")
         test_post = Post.objects.create(category_id=1,title="Post title",excerpt="Post excerpt",content="Post content",slug="post-title",author_id=1,status="published",)
-            
+    # function to create our test and run them
+    def test_blog_content(self):
+        post= Post.postobjects.get(id=1)
+        cat= Category.objects.get(id=1) 
+        author =f'{post.author}'  
+        excerpt =f'{post.excerpt}'  
+        title =f'{post.title}'  
+        content =f'{post.content}'  
+        status =f'{post.status}'  
+        self.assertEqual(author,'test_user1') # this command allow autor to be equal to test_user1
